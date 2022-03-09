693756903ce804f5ed4d1bb2fa21e9a36f5aae12
GAEUN SHIN <newsilver1028@naver.com>
2022-03-09 19:19:11 +0900

    Fix typo DiscountsMenu coomponent

--- src/Components/DiscountsMenu.tsx
+++ src/Components/DiscountsMenu.tsx
@@ -5,26 +5,19 @@ import { Discounts } from '../Reducers/cartReducer';
 
 export default function DiscountsMenu(props: { id: string; name: string }) {
   const { discounts } = useSelector((store: RootState) => store.discountReducer);
-  console.log("discounts",discounts)
-  const id = props.id;
-  const target = props.name;
-  const temp = discounts.filter((discount: Discounts) => discount.name === target)[0];
-  const menu = temp.discountedMenu;
-  const render = menu.map((el: any) => {
+  const {id, name} = props;
+  const discountsArray = discounts.filter((discount: Discounts) => discount.name === name)[0];
+  const discountedMenu = discountsArray.discountedMenu;
+  const $discountedList = discountedMenu.map((el: any) => {
     const {name, _, excludedPrice } = el;
-    return (<div>
-    <span>{name}</span>
-    <span>{excludedPrice}</span>
+    return (
+    <div key={id+name}>
+      <span>{name}</span>
+      <span>{excludedPrice}</span>
     </div>);
   })
-  console.log(temp, menu);
-  const {name, _, excludedPrice } = menu;
 
   return (
-    <>
-    {/* <span>{name}</span>
-    <span>{excludedPrice}</span> */}
-    {render}
-    </>
+    <div>{$discountedList}</div>
   )  
 }
